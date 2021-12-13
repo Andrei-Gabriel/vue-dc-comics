c27fffaf1efa26dc73d6e5681784e9a00ef1d525
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-12-13 19:22:42 +0100

    FIne footer-top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-      
-  </div>
+  <footer>
+      <div class="footer-top">
+          <div class="footer-top-container">
+              <div class="footer-top-links">
+                  <img src="../assets/img/buy-comics-digital-comics.png" alt="Digital Comics">
+                  <span>digital comics</span>
+              </div>
+              <div class="footer-top-links">
+                  <img src="../assets/img/buy-comics-merchandise.png" alt="Merchandise">
+                  <span>comics merchandise</span>
+              </div>
+              <div class="footer-top-links">
+                  <img src="../assets/img/buy-comics-subscriptions.png" alt="Subscriptions">
+                  <span>subscriptions</span>
+              </div>
+              <div class="footer-top-links">
+                  <img src="../assets/img/buy-comics-shop-locator.png" alt="Location">
+                  <span>comics shop location</span>
+              </div>
+              <div class="footer-top-links">
+                  <img id="piuPiccola" src="../assets/img/buy-dc-power-visa.svg" alt="Visa">
+                  <span>dc power visa</span>
+              </div>
+          </div>
+      </div>
+  </footer>
 </template>
 
 <script>
@@ -10,6 +33,6 @@
     }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+    // @import '../assets/style/global.scss';
 </style>
